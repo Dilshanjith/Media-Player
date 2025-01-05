ed0d59d567da26d9d4434c9cd35213d3f0107802
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2025-01-05 09:38:08 +0530

    initial commit

---
+++ src/Controller.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import java.awt.event.ActionEvent;
+
+public class Controller {
+
+    @FXML
+    void btnclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OpenSongMenu(ActionEvent event) {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(Main.class.getResource("sample.fxml"));
+        primaryStage.setTitle("MyvideoPlayer");
+        primaryStage.setScene(new Scene(root, 801, 448));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
